d8dedd02f4256585a589c39fe9d73a104da1bca8
Nathan <cowfields@gmail.com>
2014-11-07 20:57:22 +0000

    font comments

--- functions.php
+++ functions.php
@@ -200,10 +200,15 @@ external fonts. If you're using Google Fonts, you
 can replace these fonts, change it in your scss files
 and be up and running in seconds.
 */
-function bones_fonts() {
-  wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
-}
 
+function bones_fonts() {
+  /*
+  Add font declarations here
+    eg from Gfonts:
+      wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+
+  */
+}
 add_action('wp_enqueue_scripts ', 'bones_fonts');
 
 
